7b40d023b0a255f2951f5010e5afe330aae1c20d
Romeo-Peter <romeomeshack34@gmail.com>
2020-12-14 17:21:04 +0100

    App

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const app = require("express")();
 
 app.get("/", (req, res) =>
   res.json({
-    message: "Docker is awesome, but installing on windows is no easy",
+    message: "Docker is awesome, but it's struggle installing on windows 🐋",
   })
 );
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log("docker is runnig on port 8080"));
+app.listen(port, () =>
+  console.log(`App is running on http://localhost:${port}`)
+);
